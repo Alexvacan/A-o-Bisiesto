1505c5cd3f7f0cf8e6389ee4d3b6b48c0eeba463
Alexander <alexander.vacaflores@ucb.edu.bo>
2024-08-27 12:52:42 -0400

    Verifica si los diferentes anios son divisibles entre 100, pero no entre 400, si son divisibles, entre ambos son bisiestos

--- src/anio_bisiesto.js
+++ src/anio_bisiesto.js
@@ -1,6 +1,6 @@
 function EsAnioBisiesto(anio) {
     if (anio%400===0) 
         return true; 
-    else return false;
 }
 export default EsAnioBisiesto;
+
--- src/anio_bisiesto.spec.js
+++ src/anio_bisiesto.spec.js
@@ -4,4 +4,16 @@ describe("Bisiesto", () => {
   it("deberia verificar si el numero 2000 es bisiesto", () => {
     expect(EsAnioBisiesto(2000)).toEqual(true);
   });
+  it("deberia verificar si el numero 1700 es bisiesto", () => {
+    expect(EsAnioBisiesto(1700)).toEqual(false);
+  });
+  it("deberia verificar si el numero 1800 es bisiesto", () => {
+    expect(EsAnioBisiesto(1800)).toEqual(false);
+  });
+  it("deberia verificar si el numero 1900 es bisiesto", () => {
+    expect(EsAnioBisiesto(1900)).toEqual(false);
+  });
+  it("deberia verificar si el numero 2100 es bisiesto", () => {
+    expect(EsAnioBisiesto(2100)).toEqual(false);
+  });
 });
